54f91c4a49a81694d27ed4f41177dc9bfadebefe
MattiaSarti <nmattia1@gmail.com>
2020-08-31 10:49:23 +0200

    ADD - api to get topics and skills

--- course_app/views.py
+++ course_app/views.py
@@ -42,3 +42,22 @@ class CourseDetailsView(generics.RetrieveAPIView):
             cache_ttl = constants['CACHE_TTL']['SHORT']
             cache.set(self.cache_key, queryset, timeout=cache_ttl)
             return queryset
+
+
+class CourseTopicsAndSkillsListView(generics.ListAPIView):
+    """
+    Use this endpoint to GET all courses' topics and skills, without additional fields.
+    """
+    model = models.CourseModel
+    serializer_class = serializers.CourseTopicsAndSkillsSerializer
+    cache_key = 'course-list'
+
+    def get_queryset(self):
+        queryset = cache.get(self.cache_key, None)
+        if queryset:
+            return queryset
+        else:
+            queryset = self.model.objects.all()
+            cache_ttl = constants['CACHE_TTL']['SHORT']
+            cache.set(self.cache_key, queryset, timeout=cache_ttl)
+            return queryset   
