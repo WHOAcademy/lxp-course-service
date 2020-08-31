f4c76105baa5c675cd37793f8f13353252529d65
MattiaSarti <59971270+MattiaSarti@users.noreply.github.com>
2020-08-31 11:38:07 +0200

    Feature/get course details (#10)
    * ADD - api to get topics and skills
    
    * 👨‍🌾 ADD - API for all courses' topics and skills 👨‍🌾
    
    * 🍽 REMOVE - exaggerated migrations 🍽

--- course_app/serializers.py
+++ course_app/serializers.py
@@ -5,4 +5,10 @@ from .models import CourseModel
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = CourseModel
-        fields = '__all__'
+        exclude = ('course_topics', 'novice_skills', 'intermediate_skills', 'expert_skills')
+
+
+class CourseTopicsAndSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourseModel
+        fields = ['id', 'course_topics', 'novice_skills', 'intermediate_skills', 'expert_skills']
